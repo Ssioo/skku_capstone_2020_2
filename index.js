e9e00688d512c23535ac8af0ee046533c0783be9
Ssioo <wooisso@naver.com>
2020-12-07 17:09:44 +0900

    remove unused dependencies for android

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import { AppRegistry } from 'react-native'
 import { App } from './App'
-import codePush from 'react-native-code-push'
 import { name as appName } from './app.json'
 
 let app = App
